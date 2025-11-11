300630d02d7f8b7ec0ef5a328c528751ea8c1032
Nathalie Rosenkvist <195442716+DessieRose@users.noreply.github.com>
2025-11-11 12:46:09 +0100

    Added: css files

--- header.php
+++ header.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" href="stylesheet/style.css">
+    <link rel="stylesheet" href="/stylesheet/header.css">
+    <link rel="stylesheet" href="/stylesheet/footer.css">
+
     <title>Kinforma</title>
 </head>
 <body>
