57eb33eeded9d770de5d7a0680a9fbac1b6615df
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-11-12 14:25:05 +0100

    Updated: footer layout and header styles, add images

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
 <footer>
-    <ul>
-        <li><a href="#">Mer om oss</a></li>
-        <hr>
-        <li><a href="#">Behöver du hjälp?</a></li>
-        <hr>
-        <li><a href="#">Våra samarbeten</a></li>
-        <hr>
-    </ul>
+    <div class="footer-om-oss">
+        <ul>
+            <li class="content">
+                <a href="#">Mer om oss 
+                    <img src="/components/icons/arrows/arrow-link.svg" alt="arrow">
+                </a>
+            </li>
+
+            <hr>
+            <li><a href="#">Behöver du hjälp?</a></li>
+            <hr>
+            <li><a href="#">Våra samarbeten</a></li>
+            <hr>
+        </ul>
+    </div>
     <div class="social-media">
         <a href="#"><img src="/components/icons/social-media/facebook.svg" alt="facebook-logo"></a>
         <a href="#"><img src="/components/icons/social-media/instagram.svg" alt="instagram-logo"></a>
@@ -17,15 +24,16 @@
         <a href="#">Sverige/Sweden</a>
         <a href="#">Svenska <img src="/components/icons/arrows/small-arrow.svg"></a>
     </div>
+    <hr>
     <section class="policy">
-        <hr>
+        
         <ul>
             <li><a href="#">Policy om cookies</a></li>
             <li><a href="#">Kundvillkor</a></li>
             <li><a href="#">Sekretesspolicy</a></li>
         </ul>
-        <hr>
     </section>
+    <hr>
     <div class="footer-end">
         <a href="#">Jobb på Kinforma</a>
         <a href="#">Företag</a>
