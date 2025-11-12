d39947453ae932902f8af9c4d08389ca329df816
Timalm90 <timalm90@gmail.com>
2025-11-12 14:33:52 +0100

    pushing files

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
   <link rel="stylesheet" href="stylesheet/style.css" />
   <link rel="stylesheet" href="/stylesheet/header.css" />
   <link rel="stylesheet" href="/stylesheet/footer.css" />
+  <link rel="stylesheet" href="/stylesheet/index.css" />
+
 
   <title>Kinforma</title>
 </head>
@@ -59,15 +61,15 @@
     <ul>
       <details>
         <summary><li><a href="#">Sortera efter produkt</a></li></summary>
-        <div class="dropdown"><p>Example content here</p></div>
+        <div class="dropdown"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nesciunt dolore omnis, impedit facere est nobis adipisci qui dolor sapiente sint ea alias cupiditate! Enim perspiciatis debitis ad illo quo!</p></div>
       </details>
       <details>
         <summary><li><a href="#">Sortera efter aktivitet</a></li></summary>
-        <div class="dropdown"><p>Example content here</p></div>
+        <div class="dropdown"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus est soluta beatae unde explicabo tempore rerum sunt et asperiores non nihil tempora, ex obcaecati placeat corrupti odit hic sapiente atque?</p></div>
       </details>
       <details>
         <summary><li><a href="#">Sortera efter kvalitet</a></li></summary>
-        <div class="dropdown"><p>Example content here</p></div>
+        <div class="dropdown"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, adipisci sint tempora iure unde molestias repellendus molestiae eum maiores excepturi tempore assumenda illum aut cumque, odio quibusdam nisi dolores voluptatum!</p></div>
       </details>
     </ul>
     <hr />
@@ -78,4 +80,4 @@
 </nav>
 
 </header>
-<script src="/components/scripts/hamburgerlogic.js"></script>
+<script src="/javascript/hamburgerlogic.js"></script>
