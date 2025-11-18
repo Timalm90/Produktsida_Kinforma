6b07eea9d104de7d18be39c912643d06aefd1d61
Timalm90 <timalm90@gmail.com>
2025-11-18 14:30:51 +0100

    Updated structure

---
+++ hero.php
@@ -0,0 +1,17 @@
+  <div class="hero">
+        <div class="hero-text">
+            <h1>Framtidens <br>mode,<span> idag</span>.</h1>
+        </div>
+
+        <button>Utforska</button>
+        <button>Shoppa Nyheter</button>
+
+        <div class="hero-image">
+            <img src="/components/images/BG_motocross.png" alt="header-image">
+            </div>
+            
+
+            </div>
+        </div>
+        
+    </div>
--- index.php
+++ index.php
@@ -3,26 +3,8 @@
 ?>
 
 <main>
-    <div class="hero">
-        <div class="header-text">
-            <h1>Framtidens <br>mode,<span> idag</span>.</h1>
-            
-            <button>Utforska</button>
-            <button>Shoppa Nyheter</button>
-        </div>
-
-        <div class="hero-wrapper">
-            <div class="hero-image">
-                <img src="/components/images/BG_motocross.png" alt="header-image">
-            </div>
-            
-
-            </div>
-        </div>
-        
-    </div>
- 
-    
+  
+<?php require_once __DIR__ . "/hero.php";   ?>      
 <?php require_once __DIR__ . "/products.php";   ?>
 <?php require_once __DIR__ . "/techviz.php";   ?>
 <?php require_once __DIR__ . "/categories.php";   ?>
