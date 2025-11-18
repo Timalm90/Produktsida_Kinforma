324b433b73896771968853096639c8070e7770fc
Timalm90 <timalm90@gmail.com>
2025-11-18 14:13:01 +0100

    added new php files

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 ?>
 
 <main>
-    <div class="header-image">
+    <div class="hero">
         <div class="header-text">
             <h1>Framtidens <br>mode,<span> idag</span>.</h1>
+            
+            <button>Utforska</button>
+            <button>Shoppa Nyheter</button>
         </div>
 
         <div class="hero-wrapper">
@@ -13,29 +16,17 @@
                 <img src="/components/images/BG_motocross.png" alt="header-image">
             </div>
             
-            <!-- <div class="mountain-illustration">
-                <img src="/components/images/mountain.svg" alt="mountain-illustration"> -->
+
             </div>
         </div>
+        
     </div>
-
-
-    <section class="carousel-container">
-        <div class="carousel">
-            <img src="/components/product-images/fannypack/fannypack_v1_blue_body.jpg" class="carousel-item" alt="">
-            <img src="/components/product-images/jacka/jacka_v1_blue_sidefront.jpg" class="carousel-item" alt="">
-            <img src="/components/product-images/klocka/klocka_v4_celestialyellow_sidefront.png" class="carousel-item" alt="">
-            <img src="/components/product-images/termos/termos_v1_orange.jpg" class="carousel-item" alt="">
-        </div>
-
-        <h2>Nyheter</h2>
-    </section>
-
-
-
-
-
-
+ 
+    
+<?php require_once __DIR__ . "/products.php";   ?>
+<?php require_once __DIR__ . "/techviz.php";   ?>
+<?php require_once __DIR__ . "/categories.php";   ?>
+<?php require_once __DIR__ . "/newsletter.php";   ?>
 
 </main>
 
---
+++ products.php
@@ -0,0 +1,11 @@
+
+    <section class="carousel-container">
+        <div class="carousel">
+            <img src="/components/product-images/fannypack/fannypack_v1_blue_body.jpg" class="carousel-item" alt="">
+            <img src="/components/product-images/jacka/jacka_v1_blue_sidefront.jpg" class="carousel-item" alt="">
+            <img src="/components/product-images/klocka/klocka_v4_celestialyellow_sidefront.png" class="carousel-item" alt="">
+            <img src="/components/product-images/termos/termos_v1_orange.jpg" class="carousel-item" alt="">
+        </div>
+
+        <h2>Nyheter</h2>
+    </section>
