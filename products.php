1ec7abbb530835b2c2538e9db29d4747fe39effa
Timalm90 <timalm90@gmail.com>
2025-11-19 15:48:40 +0100

    updated products etc

--- products.php
+++ products.php
@@ -1,11 +1,12 @@
+<section class="gallery-container">
+  <div class="bubble-gallery">
 
-    <section class="carousel-container">
-        <div class="carousel">
-            <img src="/components/product-images/fannypack/fannypack_v1_blue_body.jpg" class="carousel-item" alt="">
-            <img src="/components/product-images/jacka/jacka_v1_blue_sidefront.jpg" class="carousel-item" alt="">
-            <img src="/components/product-images/klocka/klocka_v4_celestialyellow_sidefront.png" class="carousel-item" alt="">
-            <img src="/components/product-images/termos/termos_v1_orange.jpg" class="carousel-item" alt="">
-        </div>
+    <div class="gallery-bg"></div>  
+    
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
 
-        <h2>Nyheter</h2>
-    </section>
+  </div>
+</section>
