c5335ff69f7367bc230f6adabd6f13f89f41c3fb
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-11-19 15:20:26 +0100

    Updated: techviz section styles and image
    Enhanced the techviz section with improved CSS for layout and responsiveness.

--- techviz.php
+++ techviz.php
@@ -1,6 +1,6 @@
 <section>
     <div class="techviz frosted-glass">
-        <img src="/components/background-images/dotted_background.jpg" alt="background">
+        <img class="techviz-background-image" src="/components/background-images/dotted_background.jpg" alt="background">
         <div class="techviz-container">
             <div>
                 <p class="techviz-mini-text">Vårt hållbarhetsarbete</p>
@@ -11,7 +11,7 @@
             <div>
                 <p class="techviz-p">Våra mångsidiga produkter minskar konsumtion genom att fungera för flera extremsporter och hålla längre. De är fullt spårbara, enkla att återvinna och designade för minsta möjliga klimatavtryck.</p>
                 <div class="techviz-image">
-                    <img src="/components/product-images/techviz/techviz_v1_front.jpg" alt="technical image">
+                    <img src="/components/product-images/techviz/techviz_v1_front.png" alt="technical image">
                 </div>
             </div>
         </div>
