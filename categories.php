1a456c6c2f5444704250c279539e5ee94f3425bc
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-11-20 15:49:28 +0100

    Added: compass icon to categories section
    Introduced a compass icon to the categories section by updating the HTML structure and adding a new PNG asset.

--- categories.php
+++ categories.php
@@ -1,25 +1,31 @@
 <section>
-    <div class="catagories-container">
-        <a href="#" class="catagories">
-            <img src="/components/images/skiing.png" alt="Skiing">
-            <span class="cat-title button-glass">Skiing</span>
-        </a>
+    <div class="categories-wrapper">
+        
+        <div class="catagories-container">
+            
+            <a href="#" class="catagories">
+                <img src="/components/images/skiing.png" alt="Skiing">
+                <span class="cat-title button-glass">Skiing</span>
+            </a>
 
-        <a href="#" class="catagories">
-            <img src="/components/images/klättring.jpg" alt="Climbing">
-            <span class="cat-title button-glass">Climbing</span>
-        </a>
+            <a href="#" class="catagories">
+                <img src="/components/images/klättring.jpg" alt="Climbing">
+                <span class="cat-title button-glass">Climbing</span>
+            </a>
 
-        <a href="#" class="catagories">
-            <img src="/components/images/surfing_2.jpg" alt="Surfing">
-            <span class="cat-title button-glass">Surfing</span>
-        </a>
+            <a href="#" class="catagories">
+                <img src="/components/images/surfing_2.jpg" alt="Surfing">
+                <span class="cat-title button-glass">Surfing</span>
+            </a>
 
-        <a href="#" class="catagories">
-            <img src="/components/images/motokross_2.png" alt="Motocross">
-            <span class="cat-title button-glass">Motocross</span>
-        </a>
+            <a href="#" class="catagories">
+                <img src="/components/images/motokross_2.png" alt="Motocross">
+                <span class="cat-title button-glass">Motocross</span>
+            </a>
 
+            <div class="compass-icon">
+                <img src="/components/icons/kompass_icon.png" alt="compass-icon">
+            </div>
+        </div>
     </div>
-
 </section>
