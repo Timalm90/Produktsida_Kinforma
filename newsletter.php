11dead7e3201c8172800a79284828f5968fa15f9
Timalm90 <timalm90@gmail.com>
2025-11-22 08:07:49 +0100

    skeleton for newsletter page

--- newsletter.php
+++ newsletter.php
@@ -1,40 +1,40 @@
 <section>
-</div class="outer">
-   <div class="largeText">Vad kan jag få som medlem?</div>
-    <div class="news-container">
-        <div class="bubble-block">
-            <div class="news-bubble bubble-glass">
-                <img class="news-img" src="/components/product-images/fannypack/Blue/1.png">
+    
+   <div class="news-container">
+
+        <div class ="upper">
+            <h3>Vad du kan få som medlem?</h3>
+            <div class ="upper-inner">
+                <p>Som medlem får du 
+                    en gratis kabin-nyckelring
+                    robust nog för dina 
+                    vildaste äventyr!</p>
+                <div class="news-bubble bubble-glass">
+                    <img class="news-img" src="/components/product-images/merch/carbine.png">
+                </div>            
             </div>
-            <div class="news-title">Kinforma T-shirt få
-med egna initialer</div>
+
+
+        </div>
+    <hr>
+        <div class="lower">
+            <h3>Som medlem har du tillgång till limited edition</h3>
+            <div class="lower-inner">
+                <p>Termos i limited edition 
+                    med specialfärg och 
+                    personliga initialer.</p>
+                <div class="news-bubble bubble-glass">
+                    <img class="news-img" src="/components/product-images/merch/lim-ed.png">
+                </div>      
+            </div>          
+     
+
+
         </div>
 
 
-        <div class="bubble-block">
-            <div class="news-bubble bubble-glass">
-                <img class="news-img" src="/components/product-images/jacket/Blue/2.png">
-            </div>
-            <div class="news-title">Kinforma Väska
-med egna initialer</div>
-        </div>
+     </div>
 
-        <div class="bubble-block">
-            <div class="news-bubble bubble-glass">
-                <img class="news-img" src="/components/product-images/smartwatch/Sand/2.png">
-            </div>
-            <div class="news-title">Kinforma nåt mer</div>
-  
-        </div>
 
-        <div class="bubble-block">
-            <div class="news-bubble bubble-glass">
-                <img class="news-img" src="/components/product-images/fannypack/Green/2.png">
-            </div>
-            <div class="news-title">Kinforma nåt mer</div>
-            
-        </div>
 
-    </div>
-</div>
 </section>
