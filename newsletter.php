b4de5f669765b664ae15d8867c4be77771c9b6dd
Timalm90 <timalm90@gmail.com>
2025-11-24 22:44:05 +0100

    small fix to bubble-glass, fixed newsletter layout

--- newsletter.php
+++ newsletter.php
@@ -1,40 +1,61 @@
 <section>
-    
-   <div class="news-container">
+    </div class="newsletter">
 
-        <div class ="upper">
+    <!--   Registration Box   -->
+    <div class="registration-container">
+
+        <div class="reg-textblock">
+        <h3>Gå med i Kinforma community</h3>
+        <br>
+        <p>För exklusiva limited edition produkter!</p>
+        </div>    
+    </br>
+        <input type="text" name="email" placeholder="Ange din e-postadress*" class="register-mail">
+
+    <br>
+        <div class="reg-buttons">
+        <button class="register">Gå med</button>
+        <button class="unregister">Avregistrera mig</button>
+        </div>    
+    </div>
+    </div>
+
+    <!--   Member content   -->
+    <div class="content-container">
+
+        <div class="upper">
             <h3>Vad du kan få som medlem?</h3>
-            <div class ="upper-inner">
-                <p>Som medlem får du 
+            <div class="upper-inner">
+                <p>Som medlem får du
                     en gratis kabin-nyckelring
-                    robust nog för dina 
+                    robust nog för dina
                     vildaste äventyr!</p>
                 <div class="news-bubble bubble-glass">
                     <img class="news-img" src="/components/product-images/merch/carbine.png">
-                </div>            
+                </div>
             </div>
-
-
         </div>
-    <hr>
+
+        <hr>
+
         <div class="lower">
             <h3>Som medlem har du tillgång till limited edition</h3>
             <div class="lower-inner">
-                <p>Termos i limited edition 
-                    med specialfärg och 
+                <p>Termos i limited edition
+                    med specialfärg och
                     personliga initialer.</p>
                 <div class="news-bubble bubble-glass">
                     <img class="news-img" src="/components/product-images/merch/lim-ed.png">
-                </div>      
-            </div>          
-     
+                </div>
+            </div>
+
 
 
         </div>
 
 
-     </div>
+    </div>
 
 
-
+    </div>
 </section>
